e309213be8a308d1e36fb4df8e9f2d7dd6e35582
Mark Wisdom Reeves <46942868+markwisdomreeves@users.noreply.github.com>
2021-06-09 17:58:16 +0200

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -28,7 +28,7 @@ class Navbar extends Component {
 
           <div className="profile-info-box">
             <h3>Mark Wisdom Reeves</h3>
-            <p>Full Stack Web Developer</p>
+            <p>Junior Full Stack Web Developer</p>
           </div>
 
           <div className="profile-vertical-line"></div>
