e12284553d48ff504dc2902bf2632fb0a64f6b99
Mark Wisdom Reeves <46942868+markwisdomreeves@users.noreply.github.com>
2021-08-05 17:20:53 +0200

    Update EducationSection.js

--- src/components/education/EducationSection.js
+++ src/components/education/EducationSection.js
@@ -15,7 +15,7 @@ const EducationScreen = ({title, id}) => {
               <h2>Full Stack Web Development Program</h2>
               <div className="cert-link-box">
                 <h3>Acheivement: </h3>
-                <a href="https://sites.google.com/view/mark-treehouse-tech-degree" className="cert-link-style" rel="noreferrer" target="_blank">Tech Degree</a>
+                <a href="https://sites.google.com/view/mark-treehouse-tech-degree" className="cert-link-style" rel="noreferrer" target="_blank">View Tech Degree</a>
               </div>
               <a href="https://teamtreehouse.com" className="tech-school-link" rel="noreferrer" target="_blank">Team Tree House</a>
               <h4>Year: Dec 12, 2019 – Sept 09, 2020</h4>
@@ -29,7 +29,7 @@ const EducationScreen = ({title, id}) => {
               <h2>JavaScript & Data Structures Course</h2>
               <div className="cert-link-box">
                 <h3>Acheivement: </h3>
-                <a href="https://sites.google.com/view/mark-javascript" className="cert-link-style" rel="noreferrer" target="_blank">Certificate</a>
+                <a href="https://sites.google.com/view/mark-javascript" className="cert-link-style" rel="noreferrer" target="_blank">View Certificate</a>
               </div>
               <a href="https://www.freecodecamp.org" className="tech-school-link" rel="noreferrer" target="_blank">Free Code Camp</a>
               <h4>Year: Aug 08, 2020 – Oct 10, 2020</h4>
@@ -43,7 +43,7 @@ const EducationScreen = ({title, id}) => {
               <h2>Responsive Web Development Course</h2>
               <div className="cert-link-box">
                 <h3>Acheivement: </h3>
-                <a href="https://sites.google.com/view/markwisdomreeves" className="cert-link-style" rel="noreferrer" target="_blank">Certificate</a>
+                <a href="https://sites.google.com/view/markwisdomreeves" className="cert-link-style" rel="noreferrer" target="_blank">View Certificate</a>
               </div>
               <a href="https://www.freecodecamp.org" className="tech-school-link" rel="noreferrer" target="_blank">Free Code Camp</a>
               <h4>Year: Mar 03, 2020 – Jun 06, 2020</h4>
